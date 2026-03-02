c01cafe2a29a1ce9825bd92ab3f3a7197e8400e5
Arafat Rahman Sijan <ar.sijan.official@gmail.com>
2026-03-03 04:49:22 +0600

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import viteCompression from "vite-plugin-compression";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteCompression({ algorithm: "brotliCompress" }),
+  ],
+  build: {
+    chunkSizeWarningLimit: 600, // increase warning limit if needed
+    rollupOptions: {
+      output: {
+        // Split vendor libraries into a separate chunk
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
